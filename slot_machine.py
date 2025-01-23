df56adcc3ff1b6793b2850076fe6a6c2d3f275fb
John Doe <Jacksonzoo@users.noreply.github.com>
2025-01-23 16:04:27 -0500

    Defined a function to store the symbols for the slot machine

--- slot_machine.py
+++ slot_machine.py
@@ -20,6 +20,20 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(frequency):
             all_symbols.append(symbol)
 
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    
+    return columns
+
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
